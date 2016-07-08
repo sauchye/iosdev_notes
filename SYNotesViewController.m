57d6439c46c8a31370cee15b09682f3e9f08edd9
Sauchye <sauchyer@gmail.com>
2016-07-08 12:15:51 +0800

    add

---
+++ SYNotesViewController.m
@@ -0,0 +1,24 @@
+//
+//  SYNotesViewController.m
+//
+//  Created by Saucheong Ye on 7/24/15.
+//  Copyright (c) 2015 sauchye.com. All rights reserved.
+//
+
+
+#import "ViewController.h"
+
+@interface SYNotesViewController ()
+
+@end
+
+@implementation SYNotesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"Hi iOS Dev Notes");
+    
+}
+
+@end
